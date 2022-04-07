604c933fc66406f3e45c4d4b661e842b2b0aee77
Anna Go <annago@Annas-MacBook-Pro.local>
2022-04-06 19:23:53 -0700

    updated the docstrings

--- condensed_sparse.py
+++ condensed_sparse.py
@@ -7,26 +7,24 @@ from torch.nn.parameter import Parameter
 
 def _gen_indx_seqs(fan_in: int, num_out: int, num_in: int, fan_out_const: bool) -> torch.LongTensor:
     """
-    Generates indices for drawing recombination vectors from the input vector v.
-        
-        number recomb.vectors = fan_in (also called k)
-        length of each recomb.vector = num_out (= n2)
+    Generates indices required by the condensed layer (LinearCondensed) for drawing recombination vectors from the input vector v.
 
     Args:
-        fan_in: Number of incoming connections for each neuron.
-        num_out: Number of neurons in the current layer.
-        num_in: TODO.
-        fan_out_const: If True, nearly constant fan-out will be ensured.
+        fan_in: Number of recombination vectors, corresponding to the number of columns in the weight matrix of LinearCondensed.
+        num_out: Length of recombination vectors, corresponding to the number of rows in the weight matrix of LinearCondensed.
+        num_in: Length of the input vector(s).
+        fan_out_const: If True, nearly constant fan-out will be ensured. Nearly, and not exactly, because in some cases the number of connections is not evenly divisible by the number of neurons.
         
     Returns:
-        A 2d array of indices of the same shape as weights.
+        A 2d array of indices of the same shape as the weight matrix in LinearCondensed, namely (num_out, fan_in).
     """
+
     indx_seqs = np.zeros((num_out, fan_in))
 
-    # indices of v (the input of length d)
+    # indices of input vector
     v_inds = np.arange(num_in)
 
-    # initialize an array of probabs for every index of v (initially uniform)
+    # initializing an array of probabilities for every index of v (initially uniform)
     probs = 1/num_in*np.ones(num_in)
 
     for row_nr in range(num_out):
@@ -44,16 +42,16 @@ def _gen_indx_seqs(fan_in: int, num_out: int, num_in: int, fan_out_const: bool) 
 
 class LinearCondensed(nn.Module):
     r"""Applies a special condensed matmul
-    transformation to the incoming data: :math:`y = xA^T + b`
+    transformation to the incoming data.
 
     This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
 
     Args:
-        in_features: size of each input sample
-        out_features: size of each output sample
-        fan_in: the number of incoming connections for each neuron in the layer.
+        in_features: Length of each input vector.
+        out_features: Length of layer output.
+        fan_in: The number of rows in the weight matrix.
         bias: If set to ``False``, the layer will not learn an additive bias.
-            Default: ``True``
+            Default: ``True``.
 
     Shape:
         - Input: :math:`(*, H_{in})` where :math:`*` means any number of
@@ -63,9 +61,9 @@ class LinearCondensed(nn.Module):
 
     Attributes:
         weight: the learnable weights of the module of shape
-            :math:`(\text{out\_features}, \text{in\_features})`. The values are
+            :math:`(\text{out\_features}, \text{fan\in})`. The values are
             initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
-            :math:`k = \frac{1}{\text{in\_features}}`
+            :math:`k = \frac{1}{\text{fan\in}}`
         bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
                 If :attr:`bias` is ``True``, the values are initialized from
                 :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
@@ -73,11 +71,11 @@ class LinearCondensed(nn.Module):
 
     Examples::
 
-        >>> m = nn.Linear(20, 30)
-        >>> input = torch.randn(128, 20)
+        >>> m = nn.LinearCondensed(20, 10, 5, False)
+        >>> input = torch.randn(64, 784)
         >>> output = m(input)
         >>> print(output.size())
-        torch.Size([128, 30])
+        torch.Size([64, 10])
     """
     __constants__ = ['in_features', 'out_features']
     in_features: int
@@ -101,8 +99,7 @@ class LinearCondensed(nn.Module):
         self.reset_parameters()
 
         # ===== INDICES FOR RECOMBS =====
-        self.indx_seqs = _gen_indx_seqs(fan_in=fan_in, num_out=out_features, num_in=in_features,
-                                        fan_out_const=fan_out_const)
+        self.indx_seqs = _gen_indx_seqs(fan_in=fan_in, num_out=out_features, num_in=in_features, fan_out_const=fan_out_const)
 
     def reset_parameters(self) -> None:
         # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
@@ -119,6 +116,6 @@ class LinearCondensed(nn.Module):
         return output
 
     def extra_repr(self) -> str:
-        return 'in_features={}, out_features={}, fan_in={}, bias={}'.format(
-            self.in_features, self.out_features, self.fan_in, self.bias is not None
+        return 'in_features={}, out_features={}, fan_in={}, fan_out_const={}, bias={}'.format(
+            self.in_features, self.out_features, self.fan_in, self.fan_out_const, self.bias is not None
         )
