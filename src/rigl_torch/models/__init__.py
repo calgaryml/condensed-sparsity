b9091d29593790ad0aed3227539c2b1c8980b47b
Mike Lasby <mklasby@gmail.com>
2022-05-31 17:29:43 +0000

    add missing import to register wideresnet

--- src/rigl_torch/models/__init__.py
+++ src/rigl_torch/models/__init__.py
@@ -1,4 +1,4 @@
 from .mnist import MnistNet  # noqa
-from ._utils import get_model  # noqa
 from .model_factory import ModelFactory  # noqa
 from .resnet import ResNet18  # noqa
+from .wide_resnet import WideResNet  # noqa
