f5012acf342b7ddee5fa8a405cba920d322b5ef8
Mike Lasby <mklasby@gmail.com>
2022-06-09 20:45:05 +0000

    more plot results

--- scripts/complete_cifar_sweep.sh
+++ scripts/complete_cifar_sweep.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # resnet18 models
 #benchmark -> sgd, adadelta w/ weight decay, adamW
-python train_rigl.py \
-    dataset=cifar10 \
-    model=resnet18 \
-    rigl.dense_allocation=0.3 \
-    rigl.const_fan_in=True \
-    training.seed=6037
+# python train_rigl.py \
+#     dataset=cifar10 \
+#     model=resnet18 \
+#     rigl.dense_allocation=0.3 \
+#     rigl.const_fan_in=True \
+#     training.seed=6037
 
-python train_rigl.py \
-    dataset=cifar10 \
-    model=resnet18 \
-    rigl.dense_allocation=0.3 \
-    rigl.const_fan_in=True \
-    training.seed=8746
+# python train_rigl.py \
+#     dataset=cifar10 \
+#     model=resnet18 \
+#     rigl.dense_allocation=0.3 \
+#     rigl.const_fan_in=True \
+#     training.seed=8746
 
 python train_rigl.py \
     dataset=cifar10 \
