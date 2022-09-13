633580b238b9efafa7da8b7d258c8b46557f5eb4
Mike Lasby <mklasby@gmail.com>
2022-09-13 13:26:40 -0600

    slurm imagenet script and new lock file for py 3.8.10 to match cluster

---
+++ slurm/train_imagenet.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## GET RESOURCES ##
+
+#SBATCH --job-name=train_imagenet
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G    
+#SBATCH --time=1-00:00:00
+#SBATCH --mail-user=mklasby@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+## --- Migrate data to local node storage --- ##
+export SCRATCH=/scratch/mklasby/ILSVRC/
+export WORKDIR=/home/mklasby/projects/def-rmsouza/mklasby/condensed-sparsity/
+cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
+cp $SCRATCH/ILSVRC2012_devkit_t12.tar.gz $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_val.tar $SLURM_TMPDIR
+
+## SET ENV ##:
+module load singularity python/3.8.10 cuda cudnn
+source $SLURM_TMPDIR/.venv/bin/activate
+
+## RUN SCRIPT ##
+python3 ${WORKDIR}/scripts/extract_imagenet.py
