6639ed13730b7496209ed51cc2cd2dfbdef82377
Mike Lasby <mklasby@gmail.com>
2022-09-13 14:36:49 -0600

    updated imagenet slurm script

--- slurm/get-imagenet-data.sh
+++ slurm/get-imagenet-data.sh
@@ -17,7 +17,12 @@
 # wget  https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
 # wget  https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
 
-export WORKDIR=/home/mklasby/projects/def-rmsouza/mklasby/condensed-sparsity/
+export SCRATCH=/scratch/mklasby/ILSVRC
+cp $SCRATCH/ILSVRC2012_devkit_t12.tar.gz $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_val.tar $SLURM_TMPDIR
+
+export WORKDIR=/home/mklasby/projects/def-rmsouza/mklasby/condensed-sparsity
 module load singularity python/3.8.10 cuda cudnn
 source ${WORKDIR}/.venv/bin/activate
 python3 ${WORKDIR}/scripts/extract_imagenet.py >/dev/null 2>&1
