c4ab46092bda023236c45c5f92a4e356431cc3c7
Mike Lasby <mklasby@gmail.com>
2022-09-15 15:56:46 -0700

    more pinned wheels

--- scripts/build_compute_canada_venv.sh
+++ scripts/build_compute_canada_venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load singularity python/3.8.10 cuda cudnn
-rm -rf ../.venv
+rm -rf ./.venv
 virtualenv .venv
 source .venv/bin/activate
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
@@ -14,5 +14,8 @@ pip download --no-deps torch==1.12.1
 pip download --no-deps Pillow==9.2.0
 pip download --no-deps Pillow_SIMD==9.0.0.post1+computecanada
 pip download --no-deps scipy==1.8.0
+pip download --no-deps grpcio==1.47.0+computecanada
+pip download --no-deps numpy==1.23.0+computecanada
+pip download --no-deps pandas==1.4.1+computecanada
 cd ..
 poetry install -vvv
