fa32914c80227ac77757a4cb2460c3f482d6ae07
Mike Lasby <mklasby@gmail.com>
2022-09-22 11:39:34 -0700

    update from cedar

--- slurm/train_imagenet.sh
+++ slurm/train_imagenet.sh
@@ -5,23 +5,24 @@
 #SBATCH --job-name=train_imagenet
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100l:1
 #SBATCH --mem=16G    
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-user=mklasby@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --account=def-yani
 
 ## --- Migrate data to local node storage --- ##
 export SCRATCH=/scratch/mklasby/ILSVRC/
 export WORKDIR=/home/mklasby/projects/def-rmsouza/mklasby/condensed-sparsity/
-cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
+# cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
 cp $SCRATCH/ILSVRC2012_devkit_t12.tar.gz $SLURM_TMPDIR
 cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
 cp $SCRATCH/ILSVRC2012_img_val.tar $SLURM_TMPDIR
 
 ## SET ENV ##:
 module load singularity python/3.8.10 cuda cudnn
-source $SLURM_TMPDIR/.venv/bin/activate
+source ${WORKDIR}/.venv/bin/activate
 
 ## RUN SCRIPT ##
-python3 ${WORKDIR}/scripts/extract_imagenet.py
+python3 ${WORKDIR}/scripts/train_imagenet_rigl.py
