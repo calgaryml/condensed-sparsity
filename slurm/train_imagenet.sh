48efe9d93238e9c8244ad36b6fe20dcec6ded997
Mike Lasby <mklasby@gmail.com>
2022-09-22 12:10:17 -0700

    loading venv from slurm_tmpdir

--- slurm/train_imagenet.sh
+++ slurm/train_imagenet.sh
@@ -15,14 +15,14 @@
 ## --- Migrate data to local node storage --- ##
 export SCRATCH=/scratch/mklasby/ILSVRC/
 export WORKDIR=/home/mklasby/projects/def-rmsouza/mklasby/condensed-sparsity/
-# cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
+cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
 cp $SCRATCH/ILSVRC2012_devkit_t12.tar.gz $SLURM_TMPDIR
 cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
 cp $SCRATCH/ILSVRC2012_img_val.tar $SLURM_TMPDIR
 
 ## SET ENV ##:
 module load singularity python/3.8.10 cuda cudnn
-source ${WORKDIR}/.venv/bin/activate
+source ${SLURM_TMPDIR}/.venv/bin/activate
 
 ## RUN SCRIPT ##
 python3 ${WORKDIR}/train_rigl.py
