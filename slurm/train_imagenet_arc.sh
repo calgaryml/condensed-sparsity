a32fee90d7b8eeb1480e8f7b7890b238ecde3967
Mike Lasby <mklasby@ucalgary.ca>
2022-10-11 12:55:32 -0600

    updated arc training script

--- slurm/train_imagenet_arc.sh
+++ slurm/train_imagenet_arc.sh
@@ -4,15 +4,15 @@
 
 #SBATCH --job-name=train_imagenet
 #SBATCH --ntasks=1
-#SBATCH -p gpu-a100
+#SBATCH -p bigmem
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:1
-#SBATCH --mem=24G
+#SBATCH --mem=325G
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-user=mklasby@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-## SET ENV ##:
+## SET VARS & MOVE DATA TO NODE ##:
 export PATH=~/software/miniconda3/bin:$PATH
 export WORKDIR=/work/souza_lab/lasby/condensed-sparsity
 export SLURM_TMPDIR=/dev/shm
@@ -23,6 +23,7 @@ cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
 cp $SCRATCH/ILSVRC2012_img_val.tar $SLURM_TMPDIR
 cp -r $WORKDIR/.venv $SLURM_TMPDIR
 
+## SET ENV ##:
 eval "$(conda shell.bash hook)"
 conda activate py38
 source ${SLURM_TMPDIR}/.venv/bin/activate
