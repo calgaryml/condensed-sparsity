48cadeaf487e0667a2fc8a27483d2ed09e629d78
Mike Lasby <mklasby@gmail.com>
2022-12-07 14:20:00 -0800

    added slurm script to join sweep

---
+++ slurm/run_train_rigl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## GET RESOURCES ##
+
+#SBATCH --job-name=wandb_agent
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G    
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-user=mklasby@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --account=def-yani
+
+## --- Migrate venv / data to local node storage --- ##
+export WORKDIR=/home/mklasby/projects/def-yani/mklasby/condensed-sparsity/
+cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
+
+## SET ENV ##:
+module load singularity python/3.8.10 cuda/11.4 cudnn
+source ${SLURM_TMPDIR}/.venv/bin/activate
+
+## RUN SCRIPT ##
+wandb agent condensed-sparsity/condensed-rigl/1jyv1omw
