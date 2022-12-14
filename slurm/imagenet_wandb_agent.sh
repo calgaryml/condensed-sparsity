6e2e0b1e98e1842be7764675caee0cf8629630cc
Mike Lasby <mklasby@gmail.com>
2022-12-14 23:33:14 +0000

    sweep edits

--- slurm/imagenet_wandb_agent.sh
+++ slurm/imagenet_wandb_agent.sh
@@ -7,18 +7,22 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:v100l:2
 #SBATCH --mem=32G    
-#SBATCH --time=7-00:00:00
+#SBATCH --time=4-00:00:00
 #SBATCH --mail-user=mklasby@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --account=def-yani
 
 ## --- Migrate venv / data to local node storage --- ##
+export SCRATCH=/scratch/mklasby/ILSVRC/
 export WORKDIR=/home/mklasby/projects/def-yani/mklasby/condensed-sparsity/
-export WANDB_SWEEP_ID=hgqc3joo
+export WANDB_SWEEP_ID=
 cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
+cp $SCRATCH/ILSVRC2012_devkit_t12.tar.gz $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_val.tar $SLURM_TMPDIR
 
 ## SET ENV ##:
-module load singularity python/3.8.10 cuda/11.4 cudnn
+module load python/3.8.10 cuda/11.4 cudnn
 source ${SLURM_TMPDIR}/.venv/bin/activate
 
 ## RUN SCRIPT ##
