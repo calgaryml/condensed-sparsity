7b39370f6acd44d3d487d912862f277d03871df6
Mike Lasby <mklasby@gmail.com>
2022-12-21 16:48:04 +0000

    results updated with imagenet runs

--- scripts/download_sweep_table.py
+++ scripts/download_sweep_table.py
@@ -7,6 +7,7 @@ api = wandb.Api()
 runs = api.runs("condensed-sparsity/condensed-rigl")
 
 summary_list, config_list, name_list, state_list, id_list = [], [], [], [], []
+sweep_list = []
 for run in runs:
     # .summary contains the output keys/values for metrics like accuracy.
     #  We call ._json_dict to omit large files
@@ -22,6 +23,10 @@ for run in runs:
     name_list.append(run.name)
     id_list.append(run.id)
     state_list.append(run.state)
+    sweep_id = None
+    if hasattr(run.sweep, "id"):
+        sweep_id = run.sweep.id
+    sweep_list.append(sweep_id)
 
 runs_df = pd.DataFrame(
     {
@@ -30,6 +35,7 @@ runs_df = pd.DataFrame(
         "name": name_list,
         "id": id_list,
         "state": state_list,
+        "sweep_id": sweep_list,
     }
 )
 
