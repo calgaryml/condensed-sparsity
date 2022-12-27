2c781b2426414087ded22877d970ca5be3229165
Mike Lasby <mklasby@gmail.com>
2022-12-27 21:24:53 +0000

    config and slurm batch script ready for x5 exp

---
+++ slurm/imagenet_x5_wandb_agent.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## GET RESOURCES ##
+
+#SBATCH --job-name=imagenet_agent
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=v100l:4
+#SBATCH --time=14-00:00:00
+#SBATCH --mail-user=mklasby@ucalgary.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --account=def-yani
+
+## --- Migrate venv / data to local node storage --- ##
+export SCRATCH=/scratch/mklasby/ILSVRC/
+export WORKDIR=/home/mklasby/projects/def-yani/mklasby/condensed-sparsity/
+export WANDB_SWEEP_ID=f63nx0vb
+cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
+cp $SCRATCH/ILSVRC2012_devkit_t12.tar.gz $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
+cp $SCRATCH/ILSVRC2012_img_val.tar $SLURM_TMPDIR
+
+## SET ENV ##:
+module load python/3.8.10 cuda/11.4 cudnn
+source ${SLURM_TMPDIR}/.venv/bin/activate
+
+## RUN SCRIPT ##
+wandb online
+wandb agent condensed-sparsity/condensed-rigl/${WANDB_SWEEP_ID}
