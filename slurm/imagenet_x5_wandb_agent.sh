b5cace41a016793baea94e6f6f9ad20d9ec285a6
Mike Lasby <mklasby@gmail.com>
2023-01-11 18:34:41 -0800

    reverted config

--- slurm/imagenet_x5_wandb_agent.sh
+++ slurm/imagenet_x5_wandb_agent.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --gpus-per-node=v100l:4
 #SBATCH --mem=125G
-#SBATCH --time=14-00:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --mail-user=mklasby@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --account=def-yani
@@ -16,7 +16,7 @@
 ## --- Migrate venv / data to local node storage --- ##
 export SCRATCH=/scratch/mklasby/ILSVRC/
 export WORKDIR=/home/mklasby/projects/def-yani/mklasby/condensed-sparsity/
-export WANDB_SWEEP_ID=f63nx0vb
+export WANDB_SWEEP_ID=xzz62rrr
 cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
 cp $SCRATCH/ILSVRC2012_devkit_t12.tar.gz $SLURM_TMPDIR
 cp $SCRATCH/ILSVRC2012_img_train.tar $SLURM_TMPDIR
@@ -29,4 +29,3 @@ source ${SLURM_TMPDIR}/.venv/bin/activate
 ## RUN SCRIPT ##
 wandb online
 wandb agent condensed-sparsity/condensed-rigl/${WANDB_SWEEP_ID}
-# python ./train_rigl.py experiment.resume_from_checkpoint=True experiment.run_id=vchnjrf5
