067ef8697defc2e38c8ded42083e1eb192e520f9
Mike Lasby <mklasby@gmail.com>
2023-01-11 20:52:23 -0700

    fresh sweep id

--- slurm/wandb_agent.sh
+++ slurm/wandb_agent.sh
@@ -14,7 +14,7 @@
 
 ## --- Migrate venv / data to local node storage --- ##
 export WORKDIR=/home/mklasby/projects/def-yani/mklasby/condensed-sparsity/
-export WANDB_SWEEP_ID=0irf1g96
+export WANDB_SWEEP_ID=1fik36lx
 cp -r $WORKDIR/.venv $SLURM_TMPDIR/.venv
 
 ## SET ENV ##:
