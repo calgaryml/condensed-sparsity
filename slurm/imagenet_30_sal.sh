6833546eb03abbd69fb00b3b85517bff51afd2fb
Mike Lasby <mklasby@ucalgary.ca>
2023-01-14 14:48:52 -0700

    arc scripts updated

--- slurm/imagenet_30_sal.sh
+++ slurm/imagenet_30_sal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run_ids=("hkak5806" "p5n21r8q" "bbkeuxvr" "1hfmurvv")
-dense_allocs=(0.01 0.05 0.1 0.2)
+dense_allocs=(0.05 0.1 0.2)
 for da in ${dense_allocs[@]}
 do
-    sbatch ./slurm/train_imagenet.sh $da
+    sbatch ./slurm/train_imagenet_arc.sh $da
 done
--- slurm/train_imagenet_arc.sh
+++ slurm/train_imagenet_arc.sh
@@ -7,7 +7,7 @@
 #SBATCH -p gpu-v100
 #SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:2
-#SBATCH --mem=325G
+#SBATCH --mem=24G
 #SBATCH --time=1-00:00:00
 #SBATCH --mail-user=mklasby@ucalgary.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -29,4 +29,29 @@ conda activate py38
 source ${SLURM_TMPDIR}/.venv/bin/activate
 
 ## RUN SCRIPT ##
-python ${WORKDIR}/train_rigl.py
+dense_alloc=$1
+printf "Starting run with dense alloc == ${dense_alloc}\n"
+
+python3 ${WORKDIR}/train_rigl.py \
+dataset=imagenet \
+model=resnet50 \
+rigl.dense_allocation=${dense_alloc} \
+rigl.delta=800 \
+rigl.grad_accumulation_n=8 \
+rigl.min_salient_weights_per_neuron=0.5 \
+training.batch_size=512 \
+training.max_steps=256000 \
+training.weight_decay=0.0001 \
+training.label_smoothing=0.1 \
+training.seed=8746 \
+training.lr=0.2 \
+training.epochs=104 \
+training.warm_up_steps=5 \
+training.scheduler=step_lr_with_warm_up \
+training.step_size=[30,70,90] \
+training.gamma=0.1 \
+compute.world_size=2 \
+compute.distributed=True \
+rigl.use_sparse_initialization=True \
+rigl.init_method_str=grad_flow_init
+
