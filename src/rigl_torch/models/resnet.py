25e7346d9d6c79915687862e771201f2c384250e
Mike Lasby <mklasby@gmail.com>
2023-01-21 05:48:12 +0000

    cifar config update

--- src/rigl_torch/models/resnet.py
+++ src/rigl_torch/models/resnet.py
@@ -9,6 +9,7 @@ Reference:
 [1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
     Deep Residual Learning for Image Recognition. arXiv:1512.03385
 """
+from typing import List
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -98,6 +99,87 @@ class Bottleneck(nn.Module):
         return out
 
 
+# TODO
+
+# class SkinnyBlock(nn.Module):
+#     expansion = 1
+
+#     def __init__(self, in_planes, planes, stride=1):
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(
+#             in_planes,
+#             planes,
+#             kernel_size=3,
+#             stride=stride,
+#             padding=1,
+#             bias=False,
+#         )
+#         self.bn1 = nn.BatchNorm2d(planes)
+#         self.conv2 = nn.Conv2d(
+#             planes, planes, kernel_size=3, stride=1, padding=1, bias=False
+#         )
+#         self.bn2 = nn.BatchNorm2d(planes)
+
+#         self.shortcut = nn.Sequential()
+#         if stride != 1 or in_planes != self.expansion * planes:
+#             self.shortcut = nn.Sequential(
+#                 nn.Conv2d(
+#                     in_planes,
+#                     self.expansion * planes,
+#                     kernel_size=1,
+#                     stride=stride,
+#                     bias=False,
+#                 ),
+#                 nn.BatchNorm2d(self.expansion * planes),
+#             )
+
+#     def forward(self, x):
+#         out = F.relu(self.bn1(self.conv1(x)))
+#         out = self.bn2(self.conv2(out))
+#         out += self.shortcut(x)
+#         out = F.relu(out)
+#         return out
+
+
+class SkinnyResNet18(nn.Module):
+    pass
+
+
+#     def __init__(self, diet, num_blocks, num_classes=10):
+#         super(ResNet, self).__init__()
+#         # self.in_planes = 64
+#         self.diet = diet
+#         self.conv1 = nn.Conv2d(
+#             3, self.diet[0], kernel_size=3, stride=1, padding=1, bias=False
+#         )
+#         self.bn1 = nn.BatchNorm2d(self.diet[0])
+
+#         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+#         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+#         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+#         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+#         self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+#     def _make_layer(self, block, planes, num_blocks, stride):
+#         strides = [stride] + [1] * (num_blocks - 1)
+#         layers = []
+#         for stride in strides:
+#             layers.append(block(self.in_planes, planes, stride))
+#             self.in_planes = planes * block.expansion
+#         return nn.Sequential(*layers)
+
+#     def forward(self, x):
+#         out = F.relu(self.bn1(self.conv1(x)))
+#         out = self.layer1(out)
+#         out = self.layer2(out)
+#         out = self.layer3(out)
+#         out = self.layer4(out)
+#         out = F.avg_pool2d(out, 4)
+#         out = out.view(out.size(0), -1)
+#         out = self.linear(out)
+#         return out
+
+
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
@@ -136,6 +218,11 @@ class ResNet(nn.Module):
 @ModelFactory.register_model_loader(model="resnet18", dataset="cifar10")
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
+
+
+@ModelFactory.register_model_loader(model="skinny_resnet18", dataset="cifar10")
+def get_skinny_resnet18(diet: List[int]):
+    return SkinnyResNet18(diet, num_blocks=[2, 2, 2, 2])
 
 
 def ResNet34():
