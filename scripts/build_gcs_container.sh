04fb758bc2a722c59df0db8e577c99028f14de2d
Mike Lasby <mklasby@gmail.com>
2023-04-21 21:22:19 -0600

    better gcs test

--- scripts/build_gcs_container.sh
+++ scripts/build_gcs_container.sh
@@ -2,4 +2,4 @@
 
 docker build --file ./Dockerfile.gcs -t mklasby/condensed-sparsity:rigl-gcs --shm-size=16gb .
 docker push mklasby/condensed-sparsity:rigl-gcs
-docker run -itd --name gcp-test --mount 'type=bind,src=/home/mike/.config/gcloud,dst=/root/.config/gcloud' --env-file ./.env.gcs mklasby/condensed-sparsity:rigl-gcs
+docker run -itd --name gcp-test --mount 'type=bind,src=/home/mike/.config/gcloud,dst=/root/.config/gcloud' --env-file ./.env  --gpus all mklasby/condensed-sparsity:rigl-gcs python ./train_rigl.py dataset=cifar10 model=resnet18 training.dry_run=True
