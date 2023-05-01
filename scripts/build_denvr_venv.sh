38f574a02cddeb30cb3981c2d1f0b21355600f62
Mike Lasby <mklasby@gmail.com>
2023-05-01 20:27:58 +0000

    adding in prog denvr build script

---
+++ scripts/build_denvr_venv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# TODO: Check where we end up here and test this!
+cd ~
+# We add a previously generated ssh key that has been added to github already and pass it via param 1
+touch ~/.ssh/id_ed22519.pub
+echo ${1} > ~/.ssh/id_ed22519.pub
+# Clone repo and cd into it
+git clone git@github.com:calgaryml/condensed-sparsity.git
+cd condensed-sparsity
+# Build venv
+rm -rf ./.venv
+virtualenv .venv
+source .venv/bin/activate
+export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
+pip install --upgrade pip
+pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu116
+poetry config virtualenvs.options.always-copy true
+poetry install -vvv --only-root
