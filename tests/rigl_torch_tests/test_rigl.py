f9a4d80f8c4191dfeda902b73c28e681809660df
Mike Lasby <mklasby@gmail.com>
2023-05-03 18:04:11 +0000

    fixed test for get_W tuple unpacking

--- tests/rigl_torch_tests/test_rigl.py
+++ tests/rigl_torch_tests/test_rigl.py
@@ -85,11 +85,11 @@ def pruner(request, net, data_loaders):
 
 def test_lengths_of_W():
     resnet18 = torch.hub.load("pytorch/vision:v0.6.0", "resnet18", weights=None)
-    resnet18_W = get_W(resnet18)
+    resnet18_W, _, _ = get_W(resnet18)
     assert len(resnet18_W) == 21, 'resnet18 should have 21 "weight" matrices'
 
     resnet50 = torch.hub.load("pytorch/vision:v0.6.0", "resnet50", weights=None)
-    resnet50_W = get_W(resnet50)
+    resnet50_W, _, _ = get_W(resnet50)
     assert len(resnet50_W) == 54, 'resnet50 should have 54 "weight" matrices'
 
 
@@ -276,7 +276,7 @@ class TestRigLScheduler:
         os.remove(checkpoint_fn)
 
         # first make sure the original model is the same as the loaded one
-        original_W = get_W(original_model)
+        original_W, _, _ = get_W(original_model)
         assert len(original_W) == len(scheduler.W)
         for oW, nW in zip(original_W, scheduler.W):
             assert torch.equal(oW, nW)
