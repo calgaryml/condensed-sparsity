693294615c3946364e45288c836fa1f45cbad306
Mike Lasby <mklasby@gmail.com>
2023-05-05 17:06:53 +0000

    added imagetnet download script

---
+++ scripts/get_imagenet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir ILSVRC && cd ./ILSVRC
+wget  https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz
+wget  https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
+wget  https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar
