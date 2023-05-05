b54a14adf68a9271c9eeca43763c8587eefc3c43
Mike Lasby <mklasby@gmail.com>
2023-05-05 14:05:56 -0600

    updated denvr build

--- scripts/build_denvr_venv.sh
+++ scripts/build_denvr_venv.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-# TODO: Check where we end up here and test this!
+# Set up python and pip alias
 cd ~
+echo "alias python=python3" >> ~/.bashrc && echo "alias pip=pip3" >> ~/.bashrc
+exec bash
+
 # We add a previously generated ssh key that has been added to github already and pass it via param 1
-touch ~/.ssh/id_ed22519
-cat >> ~/.ssh/id_ed22519 << EOT
-${1} 
-EOT
-echo ${1} > ~/.ssh/id_ed22519
+# touch ~/.ssh/id_ed22519
+# cat >> ~/.ssh/id_ed22519 << EOT
+# ${1} 
+# EOT
+# echo ${1} > ~/.ssh/id_ed22519
+
 # Clone repo and cd into it
 git clone git@github.com:calgaryml/condensed-sparsity.git
 cd condensed-sparsity
+
 # Build venv
 rm -rf ./.venv
-sudo y | apt-get install python3.8-venv
 python -m venv .venv
 source .venv/bin/activate
-export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 pip install --upgrade pip
-pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu116
-poetry config virtualenvs.options.always-copy true
-poetry install -vvv --only-root
+pip install poetry==1.4.0
+poetry install -vvv
+cp -r ~/shared/ILSVRC ~/scratch/ILSVRC
+
+# git conifg
+git config --global user.name "Mike Lasby"
+git config --global user.email "mklasby@gmail.com"
