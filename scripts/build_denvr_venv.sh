05e8e67cbc1ddb83cc67f68c27e70e034bf1bdf2
Mike Lasby <mklasby@gmail.com>
2023-05-06 18:29:15 -0600

    vit min sal sweep

--- scripts/build_denvr_venv.sh
+++ scripts/build_denvr_venv.sh
@@ -2,7 +2,6 @@
 # Set up python and pip alias
 cd ~
 echo "alias python=python3" >> ~/.bashrc && echo "alias pip=pip3" >> ~/.bashrc
-exec bash
 
 # We add a previously generated ssh key that has been added to github already and pass it via param 1
 # touch ~/.ssh/id_ed22519
@@ -14,17 +13,19 @@ exec bash
 # Clone repo and cd into it
 git clone git@github.com:calgaryml/condensed-sparsity.git
 cd condensed-sparsity
+git checkout -t origin/vit-exp
 
 # Build venv
 export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
 rm -rf ./.venv
-python -m venv .venv
+python3 -m venv .venv
 source .venv/bin/activate
-pip install --upgrade pip
-pip install poetry==1.4.0
+pip3 install --upgrade pip
+pip3 install poetry==1.4.0
 poetry install -vvv
 cp -r ~/shared/ILSVRC ~/scratch/ILSVRC
 
 # git conifg
 git config --global user.name "Mike Lasby"
 git config --global user.email "mklasby@gmail.com"
+exec bash
