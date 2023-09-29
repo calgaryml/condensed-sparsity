ab1f94969ed1c481be8e6cfa0178017224704554
Mike Lasby <mklasby@gmail.com>
2023-09-28 22:45:15 -0600

    sync from hector @ iclr sub

---
+++ scripts/copy_art.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/yani/workspace/condensed-sparsity/artifacts/
+mkdir checkpoints
+cd checkpoints
+cp -r /home/mike/condensed-sparsity/artifacts/checkpoints/20230911_3f1ffqmr ./
+cp -r /home/mike/condensed-sparsity/artifacts/checkpoints/20230911_1mxhel1q ./
+cp -r /home/mike/condensed-sparsity/artifacts/checkpoints/20230601_nrblbn15 ./
+cp -r /home/mike/condensed-sparsity/artifacts/checkpoints/20230601_0p0wrlb0 ./
