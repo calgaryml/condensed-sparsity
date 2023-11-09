3568b1f7cc539cd3df1b01384c703ddc3783915a
Erik Schultheis <erik.schultheis@aalto.fi>
2023-11-09 18:51:53 +0200

    added cuda-based implementation

--- src/condensed_sparsity/condensed_linear.py
+++ src/condensed_sparsity/condensed_linear.py
@@ -215,6 +215,58 @@ class CondensedLinearFineGrained(nn.Module):
         )
 
 
+class FixedFanInCuda(nn.Module):
+    def __init__(
+            self, module: nn.Module, dtype: torch.typename = torch.float32,
+            transpose: bool = True, vectorize: bool = False, index_dtype: torch.typename = torch.int32
+    ):
+        super().__init__()
+        if dtype is None:
+            dtype = module.weight.dtype
+
+        self.transpose = transpose
+        with torch.no_grad():
+            active_neuron_idx = module.weight.sum(dim=1) != 0
+            fine_grained_idx = (module.weight[active_neuron_idx] != 0).to(
+                torch.bool
+            )
+            _, self.input_mask = fine_grained_idx.nonzero(as_tuple=True)
+            self.input_mask = self.input_mask.reshape(
+                shape=(module.weight[active_neuron_idx].shape[0], -1)
+            ).to(index_dtype)
+            weight = module.weight[active_neuron_idx].detach().type(dtype)
+            weight = torch.clone(
+                weight[fine_grained_idx]
+                .reshape(shape=(weight.shape[0], -1))
+                .detach()
+                .type(dtype)
+            )
+            # padding to multiple of 4
+            if vectorize:
+                pad = (self.input_mask.shape[1] + 3) // 4 * 4 - self.input_mask.shape[1]
+                self.input_mask = F.pad(self.input_mask, [0, pad])
+                weight = F.pad(weight, [0, pad])
+
+            self.condensed_weight = nn.Parameter(
+                weight,
+                requires_grad=False,
+            )
+
+            if hasattr(module, "bias"):
+                self.bias = nn.Parameter(
+                    torch.clone(
+                        module.bias[active_neuron_idx].detach().type(dtype)
+                    ),
+                    requires_grad=False,
+                )
+            else:
+                self.register_parameter("bias", None)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        from torch_sparse.ops import ffi_mul
+        return ffi_mul(input, self.condensed_weight, self.input_mask, self.bias, transpose=self.transpose)
+
+
 class CondensedLinearFineGrainedSparseOp(nn.Module):
     def __init__(
         self, module: nn.Module, dtype: torch.typename = torch.float32
