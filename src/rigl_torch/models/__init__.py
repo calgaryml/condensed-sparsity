b2c2240bcae56e8b242a94a7d6ccfca629260f2b
Mike Lasby <mklasby@gmail.com>
2024-04-11 16:13:33 -0600

    model ckpt migration code added to final results

--- src/rigl_torch/models/__init__.py
+++ src/rigl_torch/models/__init__.py
@@ -3,7 +3,6 @@ from .mnist import MnistNet  # noqa
 from .resnet import ResNet18  # noqa
 from .wide_resnet import WideResNet  # noqa
 from .vit import get_vit  # noqa
-from .condensed_linear import CondNet  # noqa
 from .imagenet_models import (  # noqa
     get_imagenet_resnet18,  # noqa
     get_imagenet_wide_resnet_22,  # noqa
